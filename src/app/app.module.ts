66a43c94beb67bf31ec45820f933c46207a63084
LuisFelipeGM <lfelipegm@magnasistemas.com.br>
2024-03-06 10:20:29 -0300

    Finalização Página Inicial da página não logada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CabecalhoComponent } from './components/nao-logado/cabecalho/cabecalho.component';
+import { HomeNlComponent } from './components/nao-logado/home-nl/home-nl.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    HomeNlComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nao-logado/home-nl/home-nl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-nl',
+  templateUrl: './home-nl.component.html',
+  styleUrl: './home-nl.component.css'
+})
+export class HomeNlComponent {
+
+}
