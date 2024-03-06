137001ebb97a3c3398f6ed401ea0ad1901f0c781
LuisFelipeGM <lfelipegm@magnasistemas.com.br>
2024-03-06 10:27:05 -0300

    Adicionando Rotas no projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeNlComponent } from './components/nao-logado/home-nl/home-nl.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeNlComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
